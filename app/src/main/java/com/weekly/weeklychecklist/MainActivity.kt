ebe790bdb0722cbde102a10452076e4b72a7571d
박상환 <hwan4789@gmail.com>
2024-02-05 09:16:41 +0900

    launcher icon 추가

--- app/src/main/java/com/weekly/weeklychecklist/MainActivity.kt
+++ app/src/main/java/com/weekly/weeklychecklist/MainActivity.kt
@@ -73,7 +73,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        runBlocking {
+        CoroutineScope(Dispatchers.IO).launch {
             //DB init
             val db = CheckListDatabaseRepository.getInstance()
             db.initDatabase(this@MainActivity)
